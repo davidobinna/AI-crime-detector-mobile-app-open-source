55f3f508b59348714e4b3a3cfd7796126283c3ba
david <davidobinna579@gmail.com>
2024-06-14 07:27:27 +0100

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { setupIonicReact } from "@ionic/react"
+import { IonButton, IonContent, IonPage, IonToolbar, setupIonicReact } from "@ionic/react"
 
 
 setupIonicReact()
@@ -7,17 +7,31 @@ function App() {
 
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="bg-white p-6 rounded-lg shadow-lg text-center">
-        <h1 className="text-2xl font-bold mb-4">Crime Detector</h1>
-        <p className="text-2xl font-bold mb-4">Tips: Click camera icon to take short</p>
-        <p className="text-sm mb-2">Project Owner: Emenike Prosper</p>
-        <p className="text-sm mb-2">School: University of Porthacourt</p>
-        <p className="text-sm mb-2">Process: The app will take a while to analyze capshot and describe the crime scene.</p>
-        <p className="text-sm mb-2">All crime scenes are recorded in our database for security purposes.</p>
-        <p className="text-sm mb-2">A deployment fee is required to keep the server running for long-term use.</p>
-      </div>
-    </div>
+    <IonPage>
+      <IonContent>
+        <div className="flex flex-col items-center justify-center h-screen bg-blue-100 p-4">
+          <div className="text-center mb-8">
+            <h1 className="text-4xl font-bold text-gray-500 mb-4">Crime Detector</h1>
+          </div>
+          <div className="bg-blue-900 p-6 rounded-lg shadow-lg text-left w-full max-w-lg">
+            <p className="text-lg text-white mb-4">Tips: Click camera icon to take short</p>
+            <p className="text-sm text-white mb-2">Project Owner: Emenike Prosper</p>
+            <p className="text-sm text-white mb-2">School: University of Porthacourt</p>
+            <p className="text-sm text-white mb-2">Process: The app will take a while to analyze capshot and describe the crime scene.</p>
+            <p className="text-sm text-white mb-2">All crime scenes are recorded in our database for security purposes.</p>
+            <p className="text-sm text-white mb-2">A deployment fee is required to keep the server running for long-term use.</p>
+          </div>
+        </div>
+      </IonContent>
+      <IonToolbar className="">
+        <div className="w-full p-4 bg-blue-100">
+          <IonButton expand="block" className="w-full h-12 text-white">
+            Continue
+          </IonButton>
+        </div>
+      </IonToolbar>
+
+    </IonPage>
   )
 }
 
